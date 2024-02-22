aa5e648a769c01b6c2ad234e8eef8d989ab50fb6
Kamara72 <kamarabangalie@gmail.com>
2024-02-22 10:27:55 +0000

    Add all the folders in the API Project

--- SportsApp.Api/Controllers/WeatherForecastController.cs
+++ SportsApp.Api/Controllers/WeatherForecastController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
 
-namespace SportsApp.Api.Controllers
+namespace SportsApp.Api.Controllers/
 {
     //[Authorize]
     [ApiController]
